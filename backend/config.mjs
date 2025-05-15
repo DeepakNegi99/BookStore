9d59c3c91e5422b3d5b2fbe04beb5630fab0f153
deepak <deepak99negi@gmail.com>
2025-05-16 01:02:18 +0530

    secondComment
    Added mongodb db with mongoose

---
+++ backend/config.mjs
@@ -0,0 +1,3 @@
+export const PORT = 5000;
+
+export const mongoDBURL = "mongodb+srv://sbunty303:utySF6WAme2WzRPv@dbprojects.gdpgiig.mongodb.net/?retryWrites=true&w=majority&appName=dbProjects";
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 import express from "express";
-import { PORT } from "./config";    
+import { PORT, mongoDBURL } from "./config.mjs";    
 
 const app = express();
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welocome to MERN Stack Tutorial');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
