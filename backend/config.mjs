0fbede67022f0ff9b8a9c5169ca51edfad1127e9
Deepak Negi <deepak99negi@gmail.com>
2025-06-19 12:28:46 +0530

    new o
    added Bookcard and BookModal

--- backend/config.mjs
+++ backend/config.mjs
@@ -1,4 +1,4 @@
 export const PORT = 5000;
 
 //export const mongoDBURL = "mongodb+srv://sbunty303:utySF6WAme2WzRPv@dbprojects.gdpgiig.mongodb.net/?retryWrites=true&w=majority&appName=dbProjects";
-export const mongoDBURL = "mongodb://localhost:27017/"; // Use this for local MongoDB
+export const mongoDBURL = "mongodb://localhost:27017/local"; // Use this for local MongoDB
